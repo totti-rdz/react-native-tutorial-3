286240671d9e9ec6a0b7f351433ca476a92db97b
totti-rdz <thorsten@achtsamkeitsakademie.de>
2022-11-02 15:52:12 +0100

    further styling MealItem

--- components/MealItem.jsx
+++ components/MealItem.jsx
@@ -1,17 +1,19 @@
-import { Image, Pressable, StyleSheet, Text, View } from "react-native";
+import { Image, Platform, Pressable, StyleSheet, Text, View } from "react-native";
 
 const MealItem = ({ title, imageUrl, duration, complexity, affordability }) => {
   return (
-    <View>
-      <Pressable>
-        <View>
-          <Image style={styles.image} source={{ uri: imageUrl }} />
-          <Text style={styles.title}>{title}</Text>
-        </View>
-        <View>
-          <Text>{duration} m</Text>
-          <Text>{complexity.toUpperCase()}</Text>
-          <Text>{affordability.toUpperCase()}</Text>
+    <View style={styles.mealItem}>
+      <Pressable android_ripple={{ color: "#ccc" }} style={({ pressed }) => (pressed ? styles.buttonPressed : null)}>
+        <View style={styles.innerContainer}>
+          <View>
+            <Image style={styles.image} source={{ uri: imageUrl }} />
+            <Text style={styles.title}>{title}</Text>
+          </View>
+          <View style={styles.details}>
+            <Text style={styles.detailItem}>{duration} m</Text>
+            <Text style={styles.detailItem}>{complexity.toUpperCase()}</Text>
+            <Text style={styles.detailItem}>{affordability.toUpperCase()}</Text>
+          </View>
         </View>
       </Pressable>
     </View>
@@ -20,7 +22,48 @@ const MealItem = ({ title, imageUrl, duration, complexity, affordability }) => {
 export default MealItem;
 
 const styles = StyleSheet.create({
-  container: {},
-  image: { width: "100%", height: 200 },
-  title: { fontWeight: "bold", textAlign: "center", fontSize: 18 },
+  mealItem: {
+    margin: 16,
+    borderRadius: 8,
+    backgroundColor: "white",
+    ...Platform.select({
+      android: {
+        elevation: 4,
+        overflow: "hidden",
+      },
+      ios: {
+        shadowColor: "black",
+        shadowOpacity: 0.25,
+        shadowOffset: { width: 0, height: 2 },
+        shadowRadius: 8,
+      },
+    }),
+  },
+  buttonPressed: {
+    opacity: 0.5,
+  },
+  innerContainer: {
+    borderRadius: 8,
+    overflow: "hidden",
+  },
+  image: {
+    width: "100%",
+    height: 200,
+  },
+  title: {
+    fontWeight: "bold",
+    textAlign: "center",
+    fontSize: 18,
+    margin: 8,
+  },
+  details: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 8,
+  },
+  detailItem: {
+    marginHorizontal: 4,
+    fontSize: 12,
+  },
 });
