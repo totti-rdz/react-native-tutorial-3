3f9843504ecc5df66f081d8b0b6d26654cc5172d
totti-rdz <thorsten@achtsamkeitsakademie.de>
2022-11-02 23:08:13 +0100

    add all details on MealDetailScreen

--- screens/MealDetailScreen.jsx
+++ screens/MealDetailScreen.jsx
@@ -1,19 +1,39 @@
 import { useLayoutEffect } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { Image, StyleSheet, Text, View } from "react-native";
+import MealDetails from "../components/MealDetails";
 
 import { MEALS } from "../data/dummy-data";
 
 const MealDetailScreen = ({ route, navigation }) => {
   const mealId = route.params.id;
+  const { title, imageUrl, duration, complexity, affordability, ingredients, steps } = MEALS.find(
+    (meal) => meal.id === mealId
+  );
 
   useLayoutEffect(() => {
-    const mealTitle = MEALS.find((meal) => meal.id === mealId).title;
-    navigation.setOptions({ title: mealTitle });
+    navigation.setOptions({ title: title });
   }, [mealId, navigation]);
 
   return (
     <View style={styles.container}>
-      <Text>MealDetailScreen - {mealId}</Text>
+      <Image source={{ uri: imageUrl }} style={styles.image} />
+
+      <Text>{title}</Text>
+      <View>
+        <MealDetails duration={duration} complexity={complexity} affordability={affordability} />
+      </View>
+      <Text>Ingredients</Text>
+      {ingredients.map((ingredient, idx) => (
+        <View key={idx}>
+          <Text>{ingredient}</Text>
+        </View>
+      ))}
+      <Text>Steps</Text>
+      {steps.map((step, idx) => (
+        <View key={idx}>
+          <Text>{step}</Text>
+        </View>
+      ))}
     </View>
   );
 };
@@ -24,5 +44,9 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
+  },
+  image: {
+    width: "100%",
+    height: 200,
   },
 });
