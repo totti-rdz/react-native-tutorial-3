9d2753edfab850e456f6fe82ac59b21e9aa304bf
totti-rdz <thorsten@achtsamkeitsakademie.de>
2022-11-04 01:41:33 +0100

    Add custom button to header to mark as favorite

--- screens/MealDetailScreen.jsx
+++ screens/MealDetailScreen.jsx
@@ -1,22 +1,32 @@
-import { useLayoutEffect } from "react";
-import { Button, Image, ScrollView, StyleSheet, Text, View } from "react-native";
+import { useLayoutEffect, useState } from "react";
+import { Image, ScrollView, StyleSheet, Text, View } from "react-native";
 import Subtitle from "../components/MealDetail/Subtitle";
 import List from "../components/MealDetail/List";
 import MealDetails from "../components/MealDetails";
 
 import { MEALS } from "../data/dummy-data";
+import IconButton from "../components/IconButton";
 
 const MealDetailScreen = ({ route, navigation }) => {
+  const [isFavorite, setIsFavorite] = useState(false);
   const mealId = route.params.id;
   const { title, imageUrl, duration, complexity, affordability, ingredients, steps } = MEALS.find(
     (meal) => meal.id === mealId
   );
 
-  const handleHeaderButtonPress = () => console.log("HeaderButton pressed");
+  const handleHeaderButtonPress = () => {
+    console.log("HeaderButton pressed");
+    setIsFavorite((current) => !current);
+  };
 
   useLayoutEffect(() => {
-    navigation.setOptions({ title: title, headerRight: () => <Button title="Tap me" onPress={handleHeaderButtonPress} /> });
-  }, [mealId, navigation]);
+    navigation.setOptions({
+      title: title,
+      headerRight: () => (
+        <IconButton icon="star" color={isFavorite ? "yellow" : "white"} onPress={handleHeaderButtonPress} />
+      ),
+    });
+  }, [mealId, navigation, isFavorite]);
 
   return (
     <ScrollView style={styles.rootContainer}>
